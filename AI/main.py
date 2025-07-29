a75012dd58393295de4d6cc7f8565f977b4e321e
Physics_is_awesome <ajcason@outlook.com>
2025-07-29 18:04:41 -0500

    Update main.py

--- AI/main.py
+++ AI/main.py
@@ -1,5 +1,7 @@
-import click
-from src.cli.cli import cli
+import sys
+     import os
+     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+     from src.cli.cli import cli
 
-if __name__ == "__main__":
-    cli()
+     if __name__ == "__main__":
+         cli()
