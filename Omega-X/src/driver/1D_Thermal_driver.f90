afa9fc75d59b72566897228e8eede62461762af4
Physics_is_awesome <ajcason@outlook.com>
2025-10-02 10:04:21 -0500

    Update 1D_Thermal_driver.f90

--- Omega-X/src/driver/1D_Thermal_driver.f90
+++ Omega-X/src/driver/1D_Thermal_driver.f90
@@ -2,10 +2,13 @@ program omega_x_driver
   use mesh
   use states
   use time_integrator
+  use io
   implicit none
 
   real(8) :: dt, t, t_end
-
+  integer :: step
+  character(len=100) :: fname
+  
   dt = 1.0d-3
   t_end = 1.0d0
   t = 0.0d0
@@ -18,5 +21,13 @@ program omega_x_driver
     t = t + dt
     print *, 't = ', t
   end do
+
+
+
+  step = step + 1
+  if (mod(step, 100) == 0) then
+    write(fname, '(A,I4.4,A)') "output/state_", step, ".csv"
+    call write_state_to_csv(fname)
+  end if
 
 end program omega_x_driver
