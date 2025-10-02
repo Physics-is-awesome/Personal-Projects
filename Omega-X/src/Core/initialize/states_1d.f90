aef534f519e5ff34b1f9049d6054a808cb6f34a9
Physics_is_awesome <ajcason@outlook.com>
2025-10-02 09:55:06 -0500

    Update and rename states.f90 to states_1d.f90

---
+++ Omega-X/src/Core/initialize/states_1d.f90
@@ -0,0 +1,40 @@
+module states
+  use mesh
+  implicit none
+  private
+  public :: m_h, rho_h, sigma_h, eta_h, T_h
+  public :: initialize_states
+
+  ! Field arrays
+  real(8), allocatable :: m_h(:)       ! Momentum density
+  real(8), allocatable :: rho_h(:)     ! Mass density
+  real(8), allocatable :: sigma_h(:)   ! Entropy density
+  real(8), allocatable :: eta_h(:)     ! Specific entropy (diagnostic)
+  real(8), allocatable :: T_h(:)       ! Temperature (diagnostic)
+
+contains
+
+  subroutine initialize_states()
+    implicit none
+    integer :: i
+
+    ! Allocate fields
+    allocate(m_h(N))
+    allocate(rho_h(N))
+    allocate(sigma_h(N))
+    allocate(eta_h(N))
+    allocate(T_h(N))
+
+    ! Set initial conditions (example: Gaussian density bump)
+    do i = 1, N
+      rho_h(i) = 1.0d0 + 0.2d0 * exp(-100.0d0 * (x_nodes(i) - 0.5d0)**2)
+      eta_h(i) = 1.0d0
+      sigma_h(i) = rho_h(i) * eta_h(i)
+      m_h(i) = 0.0d0
+      T_h(i) = compute_temperature(rho_h(i), eta_h(i))  ! Uses EOS
+    end do
+
+  end subroutine initialize_states
+
+end module states
+
