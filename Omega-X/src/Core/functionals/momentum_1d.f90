64fded679b4af599bf1d5c20c7a49a233feb7baa
Physics_is_awesome <ajcason@outlook.com>
2025-10-02 16:02:01 -0500

    Update momentum_1d.f90

--- Omega-X/src/Core/functionals/momentum_1d.f90
+++ Omega-X/src/Core/functionals/momentum_1d.f90
@@ -1,46 +1,46 @@
-
 module momentum_1d
-  use mesh_1d
-  use projection_matrix_1d
   implicit none
-  real(8), intent(in)  :: rho_h(N), m_h(N), sigma_h(N), eta_h(N), T_h(N)
-  real(8), intent(out) :: rhs(N)
+contains
 
-  real(8) :: u_h(N), mu(N)
-  real(8) :: du_proj(N), dmu_proj(N), deta_proj(N), dT_proj(N)
-  integer :: i
-  real(8), parameter :: Re = 100.0d0
+  subroutine compute_momentum_rhs(N, rho_h, m_h, sigma_h, eta_h, T_h, dx, Re, rhs)
+    integer, intent(in) :: N
+    real(8), intent(in) :: rho_h(N), m_h(N), sigma_h(N), eta_h(N), T_h(N)
+    real(8), intent(out) :: rhs(N)
+    real(8) :: u_h(N), du_proj(N), mu(N), dmu_proj(N), deta_proj(N), dT_proj(N)
+    integer :: i
 
-  ! Compute velocity
-  do i = 1, N
-    if (rho_h(i) > 1.0d-12) then
-      u_h(i) = m_h(i) / rho_h(i)
-    else
-      u_h(i) = 0.0d0
-    end if
-  end do
+    ! Compute velocity
+    do i = 1, N
+      if (rho_h(i) > 1.0d-12) then
+        u_h(i) = m_h(i) / rho_h(i)
+      else
+        u_h(i) = 0.0d0
+      end if
+    end do
 
-  ! Project derivatives
-  call apply_weak_derivative(u_h, du_proj)
+    ! Apply weak derivatives
+    call apply_weak_derivative(u_h, du_proj)
+    mu = m_h * u_h
+    call apply_weak_derivative(mu, dmu_proj)
+    call apply_weak_derivative(eta_h, deta_proj)
+    call apply_weak_derivative(T_h, dT_proj)
 
-  mu = m_h * u_h
-  call apply_weak_derivative(mu, dmu_proj)
+    ! Compute RHS
+    do i = 2, N-1
+      rhs(i) = 0.0d0
+      rhs(i) = rhs(i) - m_h(i) * du_proj(i) * dx
+      rhs(i) = rhs(i) + dmu_proj(i) * dx
+      rhs(i) = rhs(i) - rho_h(i) * deta_proj(i) * dx
+      rhs(i) = rhs(i) - sigma_h(i) * dT_proj(i) * dx
+      rhs(i) = rhs(i) - (1.0d0 / Re) * du_proj(i) * dx
+    end do
 
-  call apply_weak_derivative(eta_h, deta_proj)
-  call apply_weak_derivative(T_h, dT_proj)
+    ! Boundary conditions
+    rhs(1) = 0.0d0
+    rhs(N) = 0.0d0
 
-  ! Assemble RHS
-  do i = 2, N-1
-    rhs(i) = 0.0d0
-    rhs(i) = rhs(i) - m_h(i) * du_proj(i) * dx
-    rhs(i) = rhs(i) + dmu_proj(i) * dx
-    rhs(i) = rhs(i) - rho_h(i) * deta_proj(i) * dx
-    rhs(i) = rhs(i) - sigma_h(i) * dT_proj(i) * dx
-    rhs(i) = rhs(i) - (1.0d0 / Re) * du_proj(i) * dx
-  end do
+  end subroutine compute_momentum_rhs
 
-  ! Boundary conditions
-  rhs(1) = 0.0d0
-  rhs(N) = 0.0d0
 end module momentum_1d
+
 
