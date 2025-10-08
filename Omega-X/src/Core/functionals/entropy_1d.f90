51e018910a9453acdca746378089ad7c8f78fa58
Physics_is_awesome <ajcason@outlook.com>
2025-10-08 14:30:21 -0500

    Update entropy_1d.f90

--- Omega-X/src/Core/functionals/entropy_1d.f90
+++ Omega-X/src/Core/functionals/entropy_1d.f90
@@ -3,13 +3,13 @@ module entropy_1d
   implicit none
 contains
 
-  subroutine compute_entropy_rhs(N, sigma_h, u_h, T_h, dx, Re, Pr, gamma, rhs)
+  subroutine compute_entropy_rhs(N, sigma_h, u_h, T_h, dx, Re, Pr, gamma, rhs_sigma)
     implicit none
     ! Dummy arguments
     integer, intent(in) :: N
     real(8), intent(in) :: sigma_h(N), u_h(N), T_h(N)
     real(8), intent(in) :: dx
-    real(8), intent(out) :: rhs(N)
+    real(8), intent(out) :: rhs_sigma(N)
 
     ! Local variables
     real(8) :: flux(N), dx_u(N), dx_T(N)
@@ -32,18 +32,18 @@ contains
       heat_grad(i) = dx_T(i) / T_h(i)
       heat_prod(i) = (dx_T(i)**2) / (T_h(i)**2)
       heat_term(i) = (gamma / (gamma - 1.0d0)) * (heat_prod(i) - heat_grad(i) / dx)
-      rhs(i) = rhs(i) - (1.0d0 / Re) * visc_prod(i) * dx
-      rhs(i) = rhs(i) - (1.0d0 / (Re * Pr)) * heat_term(i) * dx
+      rhs_sigma(i) = rhs_sigma(i) - (1.0d0 / Re) * visc_prod(i) * dx
+      rhs_sigma(i) = rhs_sigma(i) - (1.0d0 / (Re * Pr)) * heat_term(i) * dx
     end do
 
     ! Divide by dx
     do i = 1, N
-      rhs(i) = -rhs(i) / dx
+      rhs_sigma(i) = -rhs_sigma(i) / dx
     end do
 
     ! Boundary conditions
-    rhs(1) = 0.0d0
-    rhs(N) = 0.0d0
+    rhs_sigma(1) = 0.0d0
+    rhs_sigma(N) = 0.0d0
 
   end subroutine compute_entropy_rhs
 
