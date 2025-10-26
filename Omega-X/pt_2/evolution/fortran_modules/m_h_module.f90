70e6742c4123e04dac42ae116ee098a191aa4b44
AJ Cason <ajcason@outlook.com>
2025-10-25 21:48:49 -0500

    pt_2

---
+++ Omega-X/pt_2/evolution/fortran_modules/m_h_module.f90
@@ -0,0 +1,15 @@
+! Automatically generated module for m_h
+module m_h_module
+  implicit none
+contains
+  subroutine compute_m_h(phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, eta_h, dm_h_dt)
+    implicit none
+    ! Declare inputs and outputs as real
+    real, intent(in) :: phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, eta_h
+    real, intent(out) :: dm_h_dt
+
+    ! Evolution equation
+    dm_h_dt = -dT_h_dx*phi_m_i*sigma_h - deta_h_dx*phi_m_i*rho_h + dphi_m_dx &
+          *m_h*u_h - du_h_dx*m_h*phi_m_i - du_h_dx*phi_m_i/Re
+  end subroutine compute_m_h
+end module
---
+++ Omega-X/pt_2/evolution/fortran_modules/sigma_h_module.f90
@@ -0,0 +1,16 @@
+! Automatically generated module for sigma_h
+module sigma_h_module
+  implicit none
+contains
+  subroutine compute_sigma_h(phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, eta_h, dsigma_h_dt)
+    implicit none
+    ! Declare inputs and outputs as real
+    real, intent(in) :: phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, eta_h
+    real, intent(out) :: dsigma_h_dt
+
+    ! Evolution equation
+    dsigma_h_dt = (Pr*(gamma - 1)*(dphi_sigma_dx*sigma_h*u_h - du_h_dx**2* &
+          phi_sigma_i/T_h) + dT_h_dx*dphi_sigma_dx*gamma/T_h - dT_h_dx**2* &
+          gamma*phi_sigma_i/T_h**2)/(Pr*(gamma - 1))
+  end subroutine compute_sigma_h
+end module
