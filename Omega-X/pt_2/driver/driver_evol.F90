1baefa81164bdbaf8462579821c826b20a0a2b8f
Physics_is_awesome <ajcason@outlook.com>
2025-11-02 16:56:03 -0600

    Create driver_evol.F90

---
+++ Omega-X/pt_2/driver/driver_evol.F90
@@ -0,0 +1,24 @@
+module evol
+  implicit none
+
+  subroutine driver_evolution()
+  ! run evolution in each program
+    compute_m_h()
+    compute_rho_h()
+    compute_sigma_h()
+    
+
+  ! calculate mass matrix
+
+
+  ! multiplly evolution and mass matrix
+
+  
+  ! Time integrator 
+
+  
+  ! create hdf5 program and add data
+
+
+
+  !
