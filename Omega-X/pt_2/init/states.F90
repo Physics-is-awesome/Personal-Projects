c6154b67ffaab4a6fd6a3bdbdf3c0f31fa8cfd37
Physics_is_awesome <ajcason@outlook.com>
2025-11-01 19:58:12 -0500

    Update states.F90

--- Omega-X/pt_2/init/states.F90
+++ Omega-X/pt_2/init/states.F90
@@ -5,13 +5,13 @@ program states
 #include "../config/dim_config.h"
 #if dim == 1
   real, allocatable :: mass(:), temp(:), entropy(:), momentum(:)
-  real, allocate :: mass(nx), temp(nx), entropy(nx), momentum(nx)
+  allocate(mass(nx), temp(nx), entropy(nx), momentum(nx))
 #elif dim == 2
   real, allocatable :: mass(:,:), temp(:,:), entropy(:,:), momentum(:,:)
-  real, allocate :: mass(nx, ny), temp(nx, ny), entropy(nx, ny), momentum(nx, ny)
+  allocate(mass(nx, ny), temp(nx, ny), entropy(nx, ny), momentum(nx, ny))
 #elif dim == 3
   real, allocatable :: mass(:,:,:), temp(:,:,:), entropy(:,:,:), momentum(:,:,:)
-  real, allocate :: mass(nx, ny, nz), temp(nx, ny, nz), entropy(nx, ny, nz), momentum(nx, ny, nz)
+  allocate(mass(nx, ny, nz), temp(nx, ny, nz), entropy(nx, ny, nz), momentum(nx, ny, nz))
 #endif
   call read_file()
   call init_temp()
