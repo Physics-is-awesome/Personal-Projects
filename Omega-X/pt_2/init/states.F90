dec3783aa25c298d95924628e101af1a2a7f920f
Physics_is_awesome <ajcason@outlook.com>
2025-11-01 19:22:17 -0500

    Update states.F90

--- Omega-X/pt_2/init/states.F90
+++ Omega-X/pt_2/init/states.F90
@@ -21,14 +21,14 @@ contains
   subroutine init_temp()
     integer :: i, j, k
     if (dim == 1) then
-      if (trim(temp_dist) == "gaussian" then 
+      if (trim(temp_dist) == "gaussian") then 
         do i=1, nx
           temp(i) = normal(temp_mean, temp_var)
         end do
       end if
 
     else if (dim == 2) then 
-      if (trim(temp_dist) == "gaussian" then 
+      if (trim(temp_dist) == "gaussian") then 
         do j=1, ny
           do i=1, nx
             temp(i,j) = normal(temp_mean, temp_var)
@@ -37,7 +37,7 @@ contains
       end if
 
     else if (dim == 3) then
-      if (trim(temp_dist) == "gaussian" then 
+      if (trim(temp_dist) == "gaussian") then 
         do k=1, nz
           do j=1, ny
             do i=1, nx
