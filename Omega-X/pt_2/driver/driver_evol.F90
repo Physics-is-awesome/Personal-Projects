36db5667a935ae3ab913c79a8cc6b1723d6a9ece
Physics_is_awesome <ajcason@outlook.com>
2025-11-03 16:34:54 -0600

    Update driver_evol.F90

--- Omega-X/pt_2/driver/driver_evol.F90
+++ Omega-X/pt_2/driver/driver_evol.F90
@@ -11,8 +11,8 @@ contains
   ! get numbers from config 
     call read_file()
   ! run evolution in each program
-    F_m = call compute_m_h(Re, m_h, phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, dT_h_dx, sigma_h, eta_h, deta_h_dx, rho_h, dphi_m_dx, dphi_m_i, du_h_dx, F_m_h)
-    print*, F_m
+    F_m_h = call compute_m_h(Re, m_h, phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, dT_h_dx, sigma_h, eta_h, deta_h_dx, rho_h, dphi_m_dx, dphi_m_i, du_h_dx, F_m_h)
+    print*, F_m_h
 
   ! calculate mass matrix
     call compute_mass_matrix(p, a, b, M)
