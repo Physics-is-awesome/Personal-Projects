757915d80976558af84efd6766cb7798091c9a85
Physics_is_awesome <ajcason@outlook.com>
2025-11-02 18:29:52 -0600

    Update driver_evol.F90

--- Omega-X/pt_2/driver/driver_evol.F90
+++ Omega-X/pt_2/driver/driver_evol.F90
@@ -1,15 +1,22 @@
 module evol
+  use mass_matrix
   implicit none
-
+contains
   subroutine driver_evolution()
   ! run evolution in each program
-    compute_m_h()
-    compute_rho_h()
-    compute_sigma_h()
+
     
 
   ! calculate mass matrix
+    integer, parameter :: p = 3
+    real(8) :: M(p+1, p+1)
+    real(8) :: a, b
+    integer :: i, j
 
+    a = 0.0d0
+    b = 1.0d0
+    call compute_mass_matrix(p, a, b, M)
+    print*, M
 
   ! multiplly evolution and mass matrix
 
