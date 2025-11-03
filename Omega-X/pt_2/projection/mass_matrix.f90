8a6cf9af820a6f050d2ddce094790ac5c9477f3e
Physics_is_awesome <ajcason@outlook.com>
2025-11-02 18:22:39 -0600

    Update mass_matrix.f90

--- Omega-X/pt_2/projection/mass_matrix.f90
+++ Omega-X/pt_2/projection/mass_matrix.f90
@@ -4,8 +4,13 @@
 !===========================================================
 program mass_matrix
   implicit none
-    integer, intent(in) :: p = 1
-    real(8), intent(in) :: a = 2.8, b = 9.7    
+  integer, parameter :: p = 3
+  real(8) :: M(p+1, p+1)
+  real(8) :: a, b
+  integer :: i, j
+
+  a = 0.0d0
+  b = 1.0d0
   call compute_mass_matrix(p, a, b, M)
   print*, M
 contains
