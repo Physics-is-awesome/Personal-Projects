ce893f3f1d1cbbe4872a5d6090b83e2ef7d9ac82
Physics_is_awesome <ajcason@outlook.com>
2025-11-03 16:28:56 -0600

    Update driver_evol.F90

--- Omega-X/pt_2/driver/driver_evol.F90
+++ Omega-X/pt_2/driver/driver_evol.F90
@@ -6,11 +6,11 @@ module evol
 contains
   subroutine driver_evolution()
     real(8) :: M(p+1, p+1)
-    real(8) :: phi_m_i = 1.0, phi_rho_i = 1.0, phi_sigma_i = 10.0, u_h = 1.0, T_h= 1.0, eta_h = 1.0, F_m_h
+    real(8) :: phi_m_i = 1.0, phi_rho_i = 1.0, phi_sigma_i = 1.0, u_h = 1.0, T_h = 1.0, dT_h_dx = 1.0, sigma_h = 1.0, eta_h = 1.0, deta_h_dx = 1.0, rho_h = 1.0, dphi_m_dx = 1.0, dphi_m_i = 1.0, du_h_dx = 1.0, F_m_h
   ! get numbers from config 
     call read_file()
   ! run evolution in each program
-    F_m = call compute_m_h(phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, eta_h, F_m_h)
+    F_m = call compute_m_h(phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, dT_h_dx, sigma_h, eta_h, deta_h_dx, rho_h, dphi_m_dx, dphi_m_i, du_h_dx, F_m_h)
     print*, F_m
 
   ! calculate mass matrix
