861a92af34a113ae81852d386ad12e34f66bd0dd
Physics_is_awesome <ajcason@outlook.com>
2025-11-06 11:20:20 -0600

    Update mass_matrix.f90

--- Omega-X/pt_2/evolution/mass_matrix.f90
+++ Omega-X/pt_2/evolution/mass_matrix.f90
@@ -4,10 +4,9 @@ module mass_matrix
   implicit none
 contains 
 
-  subroutine compute_mass_matrix()
-    use quad
-    use basis 
-    real(8), allocatable :: M(:, :) ! update to be dynamically 
+  subroutine compute_mass_matrix(w_q, phi, M)
+    real(8), INTENT(IN) :: w_q(:), phi(:,:)
+    real(8), INTENT(out) allocatable :: M(:, :) ! update to be dynamically 
     integer :: i, j, q
     allocate(M(3, 3))
 
