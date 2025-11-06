2c94606943efd38aea3cbae95871a118ea63edec
Physics_is_awesome <ajcason@outlook.com>
2025-11-06 13:01:39 -0600

    Update basis.F90

--- Omega-X/pt_2/projection/basis.F90
+++ Omega-X/pt_2/projection/basis.F90
@@ -6,8 +6,9 @@ contains
 
   subroutine basis_functions(xi_q, phi)
     real(8), INTENT(IN) :: xi_q(3)
-    real(8), INTENT(OUT) :: phi(3,3)
+    real(8), INTENT(OUT), allocatable :: phi(:,:)
     integer :: q
+    allocate(phi(3,3))
     do q = 1, 3
       phi(1,q) = 0.5d0 * xi_q(q) * (xi_q(q) - 1.0d0)
       phi(2,q) = 1.0d0 - xi_q(q)**2
