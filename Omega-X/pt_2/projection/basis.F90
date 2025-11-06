ed06770a0c6dc6705ae236611d3b854718d65ae3
Physics_is_awesome <ajcason@outlook.com>
2025-11-05 19:12:48 -0600

    Update and rename mass_matrix.F90 to basis.F90

---
+++ Omega-X/pt_2/projection/basis.F90
@@ -0,0 +1,15 @@
+module basis
+  use quad
+  implicit none
+
+contains
+
+  subroutine basis_functions()
+    real(8) :: xi_q(3), phi(3,3)
+    call quadrature_points()
+    do q = 1, 3
+      phi(1,q) = 0.5d0 * xi_q(q) * (xi_q(q) - 1.0d0)
+      phi(2,q) = 1.0d0 - xi_q(q)**2
+      phi(3,q) = 0.5d0 * xi_q(q) * (xi_q(q) + 1.0d0)
+    end do
+  end subroutine basis_functions
