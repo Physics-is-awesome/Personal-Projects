9c3b870da8f13d609661ff26ac19174c81e4bb1e
Physics_is_awesome <ajcason@outlook.com>
2025-11-05 19:25:38 -0600

    Create mass_matrix.f90

---
+++ Omega-X/pt_2/evolution/mass_matrix.f90
@@ -0,0 +1,20 @@
+module mass_matrix
+  use quad
+  use basis
+
+contains 
+
+  subroutine mass_matrix()
+    real(8), allocatable :: M(:, :) ! update to be dynamically 
+    integer :: i, j, q
+    allocate(M(3, 3))
+
+    do i = 1, 3
+      do j = 1, 3
+        do q = 1, 3
+          M(i,j) = M(i,j) + w_q(q) * phi(i,q) * phi(j,q)
+        end do
+      end do
+    end do
+  end subroutine mass_matrix
+
