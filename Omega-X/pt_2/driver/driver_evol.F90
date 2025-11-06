fae7b62101b823f703902f46452b97f8510a34d7
Physics_is_awesome <ajcason@outlook.com>
2025-11-05 19:27:40 -0600

    Update driver_evol.F90

--- Omega-X/pt_2/driver/driver_evol.F90
+++ Omega-X/pt_2/driver/driver_evol.F90
@@ -7,9 +7,9 @@ module evol
   implicit none
 contains
   subroutine driver_evolution()
-    real(8) :: M(p+1, p+1)
-    integer :: Re = 1
-    real(8) :: m_h = 1.0,phi_m_i = 1.0, phi_rho_i = 1.0, phi_sigma_i = 1.0, u_h = 1.0, T_h = 1.0, dT_h_dx = 1.0, sigma_h = 1.0, eta_h = 1.0, deta_h_dx = 1.0, rho_h = 1.0, dphi_m_dx = 1.0, dphi_m_i = 1.0, du_h_dx = 1.0, F_m_h
+    !real(8) :: M(p+1, p+1)
+    !integer :: Re = 1
+    !real(8) :: m_h = 1.0,phi_m_i = 1.0, phi_rho_i = 1.0, phi_sigma_i = 1.0, u_h = 1.0, T_h = 1.0, dT_h_dx = 1.0, sigma_h = 1.0, eta_h = 1.0, deta_h_dx = 1.0, rho_h = 1.0, dphi_m_dx = 1.0, dphi_m_i = 1.0, du_h_dx = 1.0, F_m_h
     ! get numbers from config 
     call read_file()
 
@@ -19,15 +19,15 @@ contains
     ! call basis functions
     basis_functions()
     ! run evolution in each program, to get week form
-    call compute_m_h(Re, m_h, phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, dT_h_dx, sigma_h, eta_h, deta_h_dx, rho_h, dphi_m_dx, dphi_m_i, du_h_dx, F_m_h)
+    ! ##call compute_m_h(Re, m_h, phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, dT_h_dx, sigma_h, eta_h, deta_h_dx, rho_h, dphi_m_dx, dphi_m_i, du_h_dx, F_m_h)
   
     
   ! multiply by week form and sum for all nodes
 
 
   ! calculate mass matrix
-    call compute_mass_matrix(p, a, b, M)
-    print*, M
+    call mass_matrix()
+
 
   ! multiplly evolution and the inverse of the mass matrix
 
