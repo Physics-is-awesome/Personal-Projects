35b5e86e5320a9c02b14dbd8616d27e0c0225ef4
Physics_is_awesome <ajcason@outlook.com>
2025-11-05 19:26:27 -0600

    Update driver_evol.F90

--- Omega-X/pt_2/driver/driver_evol.F90
+++ Omega-X/pt_2/driver/driver_evol.F90
@@ -2,17 +2,28 @@ module evol
   use mass_matrix
   use read_config
   use m_h_module
+  use quad
+  use basis
   implicit none
 contains
   subroutine driver_evolution()
     real(8) :: M(p+1, p+1)
     integer :: Re = 1
     real(8) :: m_h = 1.0,phi_m_i = 1.0, phi_rho_i = 1.0, phi_sigma_i = 1.0, u_h = 1.0, T_h = 1.0, dT_h_dx = 1.0, sigma_h = 1.0, eta_h = 1.0, deta_h_dx = 1.0, rho_h = 1.0, dphi_m_dx = 1.0, dphi_m_i = 1.0, du_h_dx = 1.0, F_m_h
-  ! get numbers from config 
+    ! get numbers from config 
     call read_file()
-  ! run evolution in each program
+
+    ! call quadrature weights and nodes
+    quadrature_points()
+
+    ! call basis functions
+    basis_functions()
+    ! run evolution in each program, to get week form
     call compute_m_h(Re, m_h, phi_m_i, phi_rho_i, phi_sigma_i, u_h, T_h, dT_h_dx, sigma_h, eta_h, deta_h_dx, rho_h, dphi_m_dx, dphi_m_i, du_h_dx, F_m_h)
-    print*, F_m_h
+  
+    
+  ! multiply by week form and sum for all nodes
+
 
   ! calculate mass matrix
     call compute_mass_matrix(p, a, b, M)
