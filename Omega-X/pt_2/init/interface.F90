f7602e8ac81c2207ce7156c9c9bfa0e53af4f16a
Physics_is_awesome <ajcason@outlook.com>
2025-11-08 13:20:26 -0600

    Update interface.F90

--- Omega-X/pt_2/init/interface.F90
+++ Omega-X/pt_2/init/interface.F90
@@ -1,7 +1,7 @@
 module c_interface
 
   interface
-    subroutine c_function(fstring, length) bind(C)
+    subroutine pass_string(fstring, length) bind(C)
         use iso_c_binding
         character(kind=c_char), dimension(*) :: fstring
         integer(c_int), value :: length
