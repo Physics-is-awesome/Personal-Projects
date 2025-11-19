4a8319667b941fb1d3296b0879dd7e198a41d5ea
Physics_is_awesome <ajcason@outlook.com>
2025-11-19 16:03:04 -0600

    Update calculated_vals.F90

--- Omega-X/pt_2/init/calculated_vals.F90
+++ Omega-X/pt_2/init/calculated_vals.F90
@@ -3,7 +3,8 @@ module calc_vals
   use declare_1
   use declare_2
   implicit none
-  sigma_h = dx+1
-  print*, sigms_h
-end module calc_vals
+
   !subroutine derivative_of_temp_by_x()
+
+
+end module calc_vals
