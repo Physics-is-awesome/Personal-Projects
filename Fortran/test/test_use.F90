4f9aae69e74ab11cb60dc3ea94630b2555254f7b
Physics_is_awesome <ajcason@outlook.com>
2025-11-19 15:47:50 -0600

    Update test_use.F90

--- Fortran/test/test_use.F90
+++ Fortran/test/test_use.F90
@@ -1,13 +1,13 @@
 program test
   use use_test
   implicit none
-
+  real(8) :: d
   call math2()
   print*, d
 contains
 
   subroutine math2()
-    real(8) :: d
+
     call math()
     d = c + 2 + a
   end subroutine math2
