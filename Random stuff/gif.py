8a235aec5fc8094ae7cdd0b6859a98c29a8bee95
Physics_is_awesome <ajcason@outlook.com>
2025-11-22 17:53:16 -0600

    Update gif.py

--- Random stuff/gif.py
+++ Random stuff/gif.py
@@ -5,15 +5,15 @@ import glob
 video_picture = "picture" # or video  
 pictures_path = "/mnt/c/Users/ajcas/Pictures"
 if video_picture == "picture":
-  files = glob.glob(os.path.join(pictures_path, "gif_*"))
+  files = glob.glob(os.path.join(pictures_path, "*.png")) #gif_
   print("Found files:", files)  # Debugging
   if not files:
       raise RuntimeError("No matching files found! Check path and naming convention.")
-  images = [imageio.imread(f) for f in files]
+  #images = [imageio.imread(f) for f in files]
 
 
 
-  imageio.mimsave("output.gif", images, duration=0.5)
+  #imageio.mimsave("output.gif", images, duration=0.5)
 elif video_picture == "video":
   clip = VideoFileClip("input.mp4")
   clip = clip.subclip(0, 5)   # first 5 seconds
