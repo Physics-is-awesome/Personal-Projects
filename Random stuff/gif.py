5d60bf0a70cc698e046acacc5c136a441438d623
Physics_is_awesome <ajcason@outlook.com>
2025-11-22 18:47:46 -0600

    Update gif.py

--- Random stuff/gif.py
+++ Random stuff/gif.py
@@ -1,4 +1,3 @@
-import imageio
 from PIL import Image
 import os, glob
 
@@ -6,18 +5,14 @@ pictures_path = "/mnt/c/Users/ajcas/OneDrive/Pictures"
 files = glob.glob(os.path.join(pictures_path, "*.png"))
 files.sort()
 
-# Open first image to get target size
-base_img = Image.open(files[0])
-target_size = base_img.size  # (width, height)
+frames = [Image.open(f).convert("RGB") for f in files]
 
-images = []
-for f in files:
-    img = Image.open(f).convert("RGB")
-    img = img.resize(target_size)  # force same size
-    images.append(img)
+# Resize all frames to match the first
+target_size = frames[0].size
+frames = [f.resize(target_size) for f in frames]
 
-# Convert PIL images to numpy arrays for imageio
-images = [imageio.imread(f) for f in files]
-
-imageio.mimsave("output.gif", images, duration=0.5)
-
+frames[0].save("output.gif",
+               save_all=True,
+               append_images=frames[1:],
+               duration=500,
+               loop=0)
