f55eed1abb762d8929b9877ef4db0d35c8904fff
Physics_is_awesome <ajcason@outlook.com>
2025-11-25 16:54:09 -0600

    Update declare_vars.F90

--- Omega-X/pt_2/init/declare_vars.F90
+++ Omega-X/pt_2/init/declare_vars.F90
@@ -1,6 +1,6 @@
 module declare_1
   implicit none
   
-  real(8), allocatable :: x_q(:), w_q(:), phi(:,:), M(:,:), T_S(:), s_h(:), e(:), eta_h(:), U(:)
+  real(8), allocatable :: phi(:,:), M(:,:), T_S(:), s_h(:), e(:), eta_h(:), U(:)
 
 end module declare_1
