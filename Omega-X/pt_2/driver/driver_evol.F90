791f16ab0e60753f13328aac6f129cb99f695e36
Physics_is_awesome <ajcason@outlook.com>
2025-11-25 16:52:51 -0600

    Update driver_evol.F90

--- Omega-X/pt_2/driver/driver_evol.F90
+++ Omega-X/pt_2/driver/driver_evol.F90
@@ -14,7 +14,7 @@ contains
     !real(8) :: M(p+1, p+1)
     !integer :: Re = 1
     !real(8) :: m_h = 1.0,phi_m_i = 1.0, phi_rho_i = 1.0, phi_sigma_i = 1.0, u_h = 1.0, T_h = 1.0, dT_h_dx = 1.0, sigma_h = 1.0, eta_h = 1.0, deta_h_dx = 1.0, rho_h = 1.0, dphi_m_dx = 1.0, dphi_m_i = 1.0, du_h_dx = 1.0, F_m_h
-    real(8) :: x_L, X_R
+    !real(8) :: x_L, X_R
     integer :: i
 
     ! get numbers from config 
