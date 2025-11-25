599b5bdf5dd9b2e3714e970d99f934de17fbf1f0
Physics_is_awesome <ajcason@outlook.com>
2025-11-25 16:22:30 -0600

    Update m_h_module.F90

--- Omega-X/pt_2/evolution/fortran_modules/m_h_module.F90
+++ Omega-X/pt_2/evolution/fortran_modules/m_h_module.F90
@@ -9,7 +9,7 @@ contains
     ! allocate(F_m_h, dT_h_dx, sigma_h, deta_h_dx, phi_m_i, rho_h, dphi_m_dx, m_h, u_h, du_h_dx, Re)
 
     ! Evolution equation
-    do i, nx
+    do i = 1, nx
       F_m_h(i) = -dT_h_dx(i)*phi_m_i(i)*sigma_h(i) - deta_h_dx(i)*phi_m_i(i)*rho_h(i) + dphi_m_dx(i)* &
             m_h(i)*u_h(i) - du_h_dx(i)*m_h(i)*phi_m_i(i) - du_h_dx(i)*phi_m_i(i)/Re
     end do
