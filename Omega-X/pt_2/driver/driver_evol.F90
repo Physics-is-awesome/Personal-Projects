b66e5ab342f18a31dfb1890ddcd78439299553cf
Physics_is_awesome <ajcason@outlook.com>
2025-11-24 18:49:27 -0600

    Update driver_evol.F90

--- Omega-X/pt_2/driver/driver_evol.F90
+++ Omega-X/pt_2/driver/driver_evol.F90
@@ -16,9 +16,7 @@ contains
     !real(8) :: m_h = 1.0,phi_m_i = 1.0, phi_rho_i = 1.0, phi_sigma_i = 1.0, u_h = 1.0, T_h = 1.0, dT_h_dx = 1.0, sigma_h = 1.0, eta_h = 1.0, deta_h_dx = 1.0, rho_h = 1.0, dphi_m_dx = 1.0, dphi_m_i = 1.0, du_h_dx = 1.0, F_m_h
     real(8) :: x_L, X_R
     integer :: i
-    ! Define physical element bounds
-    x_L = 1.0d0
-    x_R = 3.0d0
+
     ! get numbers from config 
     call read_file()
 
