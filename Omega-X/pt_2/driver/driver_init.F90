18ecd22c779ead1f5e7dd40718fd62a4a9cb6e64
Physics_is_awesome <ajcason@outlook.com>
2025-12-04 13:35:51 -0600

    Update driver_init.F90

--- Omega-X/pt_2/driver/driver_init.F90
+++ Omega-X/pt_2/driver/driver_init.F90
@@ -7,10 +7,11 @@ module driver_init
   contains
   subroutine init()
   
-    call read_file()
-    call init_temp()
-    call init_momentum()
-    call init_entropy()
+    call read_file(mass_mean, temp_mean, momentum_mean, entropy_mean, mass_var, temp_var, momentum_var, entropy_var, dx, a, b, Re, gamma, mass_dist, temp_dist, momentum_dist, entropy_dist, mass, entropy, momentum, nx, ny, nz, dim_run, p)
+    init_state(s, nx, ny, nz)
+    call init_momentum(s, nx, ny, nz, momentum_mean, momentum_var, momentum_dist)
+    call init_entropy(s, nx, ny, nz, entropy_mean, entropy_var, entropy_dist)
+    call init_mass(s, nx, ny, nz, mass_mean, mass_var, mass_dist)
     call calculate_variables()
 
   end subroutine init
