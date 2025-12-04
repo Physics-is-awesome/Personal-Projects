ba070a5d36d14c40665f2af8de54f8429a606a08
Physics_is_awesome <ajcason@outlook.com>
2025-12-04 12:34:23 -0600

    Update states.F90

--- Omega-X/pt_2/init/states.F90
+++ Omega-X/pt_2/init/states.F90
@@ -1,172 +1,158 @@
 module states
-  use read_config
   implicit none
-  
-#include "../config/dim_config.h"
-#if dim == 1
-  real(8), allocatable :: rho_h(:), T_h(:), sigma_h(:), m_h(:)
 
-#elif dim == 2
-  real(8), allocatable :: rho_h(:, :), T_h(:, :), sigma_h(:, :), m_h(:, :)
-#elif dim == 3
-  real(8), allocatable :: rho_h(:, :, :), T_h(:, :, :), sigma_h(:, :, :), m_h(:, :, :)
-#endif
-contains 
+  type :: State
+    real(8), allocatable :: rho(:,:,:)
+    real(8), allocatable :: T(:,:,:)
+    real(8), allocatable :: sigma(:,:,:)
+    real(8), allocatable :: m(:,:,:)
+  end type State
 
- 
-  ! ----------------------------------------
-  ! Tempature 
-  ! ----------------------------------------
-  subroutine init_temp()
-    integer :: i, j, k
-#if dim == 1
-    allocate(T_h(nx))
-    if (trim(temp_dist) == "gaussian") then 
-      do i=1, nx
-        T_h(i) = normal(temp_mean, temp_var)
-      end do
-    end if
+contains
 
-#elif dim == 2 
-    allocate(T_h(nx, ny)
-    if (trim(temp_dist) == "gaussian") then 
-      do j=1, ny
-        do i=1, nx
-          
-          T_h(i,j) = normal(temp_mean, temp_var)
-        end do
-      end do
-    end if
+  ! Initialize the full state arrays
+  subroutine init_state(s, nx, ny, nz)
+    type(State), intent(inout) :: s
+    integer, intent(in) :: nx
+    integer, intent(in), optional :: ny, nz
+    integer :: ny_, nz_
 
-#elif dim == 3
-    allocate(T_h(nx, ny, nz))
-    if (trim(temp_dist) == "gaussian") then 
-      do k=1, nz
-        do j=1, ny
-          do i=1, nx
-            T_h(i,j, k) = normal(temp_mean, temp_var)
+    ! Defaults for missing dimensions
+    ny_ = merge(ny, 1, present(ny))
+    nz_ = merge(nz, 1, present(nz))
+
+    allocate(s%rho(nx, ny_, nz_))
+    allocate(s%T(nx, ny_, nz_))
+    allocate(s%sigma(nx, ny_, nz_))
+    allocate(s%m(nx, ny_, nz_))
+
+    s%rho   = 0.0d0
+    s%T     = 0.0d0
+    s%sigma = 0.0d0
+    s%m     = 0.0d0
+  end subroutine init_state
+
+  !================================================================
+  ! Initialize temperature field with chosen distribution 
+  !================================================================
+  subroutine init_temp(s, nx, ny, nz, temp_mean, temp_var, temp_dist)
+    type(State), intent(inout) :: s
+    integer, intent(in) :: nx
+    integer, intent(in), optional :: ny, nz
+    real(8), intent(in) :: temp_mean, temp_var
+    character(len=*), intent(in) :: temp_dist
+
+    integer :: i, j, k, ny_, nz_
+
+    ny_ = merge(ny, 1, present(ny))
+    nz_ = merge(nz, 1, present(nz))
+
+    if (.not. allocated(s%T)) allocate(s%T(nx, ny_, nz_))
+
+    if (trim(temp_dist) == "gaussian") then
+      do k = 1, nz_
+        do j = 1, ny_
+          do i = 1, nx
+            s%T_h(i,j,k) = normal(temp_mean, temp_var)
           end do
         end do
       end do
+    else if (trim(temp_dist) == "uniform") then
+      s%T_h = temp_mean
     end if
-#endif
   end subroutine init_temp
- 
-  ! ----------------------------------------
-  ! mass 
-  ! ----------------------------------------
-  subroutine init_mass()
-    integer :: i, j, k
-#if dim == 1
-    allocate(rho_h(nx))
-    if (trim(mass_dist) == "gaussian") then 
-      do i=1, nx
-        rho_h(i) = normal(mass_mean, mass_var)
-      end do
-    end if
 
-#elif dim == 2 
-    allocate(rho_h(nx, ny)
-    if (trim(mass_dist) == "gaussian") then 
-      do j=1, ny
-        do i=1, nx
-          
-          rho_h(i,j) = normal(mass_mean, mass_var)
-        end do
-      end do
-    end if
 
-#elif dim == 3
-    allocate(rho_h(nx, ny, nz))
-    if (trim(mass_dist) == "gaussian") then 
-      do k=1, nz
-        do j=1, ny
-          do i=1, nx
-            rho_h(i,j, k) = normal(mass_mean, mass_var)
+  !================================================================
+  ! Initialize momentum field with chosen distribution 
+  !================================================================
+  subroutine init_momentum(s, nx, ny, nz, momentum_mean, momentum_var, momentum_dist)
+    type(State), intent(inout) :: s
+    integer, intent(in) :: nx
+    integer, intent(in), optional :: ny, nz
+    real(8), intent(in) :: momentum_mean, momentum_var
+    character(len=*), intent(in) :: momentum_dist
+
+    integer :: i, j, k, ny_, nz_
+
+    ny_ = merge(ny, 1, present(ny))
+    nz_ = merge(nz, 1, present(nz))
+
+    if (.not. allocated(s%T)) allocate(s%m_h(nx, ny_, nz_))
+
+    if (trim(temp_dist) == "gaussian") then
+      do k = 1, nz_
+        do j = 1, ny_
+          do i = 1, nx
+            s%m_h(i,j,k) = normal(momentum_mean, momentum_var)
           end do
         end do
       end do
+    else if (trim(momentum_dist) == "uniform") then
+      s%m_h = momentum_mean
     end if
-#endif
-  end subroutine init_mass
-
-
- 
-  ! ----------------------------------------
-  ! Momentum 
-  ! ----------------------------------------
-  subroutine init_momentum()
-    integer :: i, j, k
-#if dim == 1
-    allocate(m_h(nx))
-    if (trim(momentum_dist) == "gaussian") then 
-      do i=1, nx
-        m_h(i) = normal(momentum_mean, momentum_var)
-      end do
-    end if
-
-#elif dim == 2 
-    allocate(m_h(nx, ny)
-    if (trim(momentum_dist) == "gaussian") then 
-      do j=1, ny
-        do i=1, nx
-          
-          m_h(i,j) = normal(momentum_mean, momentum_var)
-        end do
-      end do
-    end if
-
-#elif dim == 3
-    allocate(m_h(nx, ny, nz))
-    if (trim(momentum_dist) == "gaussian") then 
-      do k=1, nz
-        do j=1, ny
-          do i=1, nx
-            m_h(i,j, k) = normal(momentum_mean, momentum_var)
-          end do
-        end do
-      end do
-    end if
-#endif
   end subroutine init_momentum
 
-  ! ----------------------------------------
-  ! Entropy 
-  ! ----------------------------------------
-  subroutine init_entropy()
-    integer :: i, j, k
-#if dim == 1
-    allocate(sigma_h(nx))
-    if (trim(entropy_dist) == "gaussian") then 
-      do i=1, nx
-        sigma_h(i) = normal(entropy_mean, entropy_var)
-      end do
-    end if
+  !================================================================
+  ! Initialize entropy field with chosen distribution 
+  !================================================================
+  subroutine init_entropy(s, nx, ny, nz, entropy_mean, entropy_var, entropy_dist)
+    type(State), intent(inout) :: s
+    integer, intent(in) :: nx
+    integer, intent(in), optional :: ny, nz
+    real(8), intent(in) :: entropy_mean, entropy_var
+    character(len=*), intent(in) :: entropy_dist
 
-#elif dim == 2 
-    allocate(sigma_h(nx, ny)
-    if (trim(entropy_dist) == "gaussian") then 
-      do j=1, ny
-        do i=1, nx
-          
-          sigma_h(i,j) = normal(entropy_mean, entropy_var)
-        end do
-      end do
-    end if
+    integer :: i, j, k, ny_, nz_
 
-#elif dim == 3
-    allocate(sigma_h(nx, ny, nz))
-    if (trim(entropy_dist) == "gaussian") then 
-      do k=1, nz
-        do j=1, ny
-          do i=1, nx
-            sigma_h(i,j, k) = normal(entropy_mean, entropy_var)
+    ny_ = merge(ny, 1, present(ny))
+    nz_ = merge(nz, 1, present(nz))
+
+    if (.not. allocated(s%T)) allocate(s%sigma_h(nx, ny_, nz_))
+
+    if (trim(entropy_dist) == "gaussian") then
+      do k = 1, nz_
+        do j = 1, ny_
+          do i = 1, nx
+            s%sigma_h(i,j,k) = normal(entropy_mean, entropy_var)
           end do
         end do
       end do
+    else if (trim(entropy_dist) == "uniform") then
+      s%sigma_h = entropy_mean
     end if
-#endif
   end subroutine init_entropy
+
+  !================================================================
+  ! Initialize mass field with chosen distribution 
+  !================================================================
+  subroutine init_mass(s, nx, ny, nz, mass_mean, mass_var, mass_dist)
+    type(State), intent(inout) :: s
+    integer, intent(in) :: nx
+    integer, intent(in), optional :: ny, nz
+    real(8), intent(in) :: mass_mean, mass_var
+    character(len=*), intent(in) :: mass_dist
+
+    integer :: i, j, k, ny_, nz_
+
+    ny_ = merge(ny, 1, present(ny))
+    nz_ = merge(nz, 1, present(nz))
+
+    if (.not. allocated(s%rho_h)) allocate(s%rho_h(nx, ny_, nz_))
+
+    if (trim(mass_dist) == "gaussian") then
+      do k = 1, nz_
+        do j = 1, ny_
+          do i = 1, nx
+            s%rho_h(i,j,k) = normal(mass_mean, mass_var)
+          end do
+        end do
+      end do
+    else if (trim(rho_h_dist) == "uniform") then
+      s%rho_h = rho_h_mean
+    end if
+  end subroutine init_mass
+
 
 ! function for normal distribution(without special start)
   function normal(mu, sigma) result(val)
