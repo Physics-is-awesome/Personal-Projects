b11c3ba5d94cc68ddca0e7b548b6ef657aeef9dd
Physics_is_awesome <ajcason@outlook.com>
2025-12-04 16:42:47 -0600

    Update states.F90

--- Omega-X/pt_2/init/states.F90
+++ Omega-X/pt_2/init/states.F90
@@ -2,10 +2,9 @@ module states
   implicit none
 
   type :: State
-    real(8), allocatable :: rho(:,:,:)
-    real(8), allocatable :: T(:,:,:)
-    real(8), allocatable :: sigma(:,:,:)
-    real(8), allocatable :: m(:,:,:)
+    real(8), allocatable :: rho_h(:,:,:)
+    real(8), allocatable :: sigma_h(:,:,:)
+    real(8), allocatable :: m_h(:,:,:)
   end type State
 
 contains
@@ -21,48 +20,16 @@ contains
     ny_ = merge(ny, 1, present(ny))
     nz_ = merge(nz, 1, present(nz))
 
-    allocate(s%rho(nx, ny_, nz_))
-    allocate(s%T(nx, ny_, nz_))
-    allocate(s%sigma(nx, ny_, nz_))
-    allocate(s%m(nx, ny_, nz_))
+    allocate(s%rho_h(nx, ny_, nz_))
+    allocate(s%sigma_h(nx, ny_, nz_))
+    allocate(s%m_h(nx, ny_, nz_))
 
-    s%rho   = 0.0d0
-    s%T     = 0.0d0
-    s%sigma = 0.0d0
-    s%m     = 0.0d0
+    s%rho_h   = 0.0d0
+    s%sigma_h = 0.0d0
+    s%m_h     = 0.0d0
   end subroutine init_state
 
-  !================================================================
-  ! Initialize temperature field with chosen distribution 
-  !================================================================
-  subroutine init_temp(s, nx, ny, nz, temp_mean, temp_var, temp_dist)
-    type(State), intent(inout) :: s
-    integer, intent(in) :: nx
-    integer, intent(in), optional :: ny, nz
-    real(8), intent(in) :: temp_mean, temp_var
-    character(len=*), intent(in) :: temp_dist
-
-    integer :: i, j, k, ny_, nz_
-
-    ny_ = merge(ny, 1, present(ny))
-    nz_ = merge(nz, 1, present(nz))
-
-    if (.not. allocated(s%T)) allocate(s%T(nx, ny_, nz_))
-
-    if (trim(temp_dist) == "gaussian") then
-      do k = 1, nz_
-        do j = 1, ny_
-          do i = 1, nx
-            s%T_h(i,j,k) = normal(temp_mean, temp_var)
-          end do
-        end do
-      end do
-    else if (trim(temp_dist) == "uniform") then
-      s%T_h = temp_mean
-    end if
-  end subroutine init_temp
-
-
+ 
   !================================================================
   ! Initialize momentum field with chosen distribution 
   !================================================================
