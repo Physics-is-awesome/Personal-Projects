000b33857a76e0b65c10d4192af6f2724664a5ef
Physics_is_awesome <ajcason@outlook.com>
2025-12-05 15:37:31 -0600

    Update quad.F90

--- Omega-X/pt_2/projection/quad.F90
+++ Omega-X/pt_2/projection/quad.F90
@@ -22,30 +22,30 @@ contains
   !   - The Jacobian J maps reference coordinates to physical coordinates.
   !   - The mapped physical points x_q are computed internally.
   !=========================================================
-  subroutine quadrature_points(nx, w_q, xi_q, x_q)
-    integer, intent(in) :: nx,  p          !(polyinomial) Number of quadrature points # change to intent in rather than parameter at later point
-    real(8), intent(out) :: w_q(:), xi_q(:), x_q(:)
+  subroutine quadrature_points(Omega)
+    type(Omega-X), intent(inout) :: Omega
+
     real(8) :: J                  ! Reference points and weights and Jacobian
     ! Mapped physical points and weights
     real(8) :: x_L, x_R            ! Element bounds 
     integer :: q
     x_L = 0.0d0
-    x_R = dble(nx)   ! convert integer nx to real
+    x_R = dble(Omega%m%nx)   ! convert integer nx to real
 
     J = (x_R - x_L) / 2.0d0              ! Jacobian for mapping
 
     ! Gauss–Legendre quadrature points and weights on [-1, 1]
-    xi_q(1) = -sqrt(3.0d0 / 5.0d0)
-    xi_q(2) =  0.0d0
-    xi_q(3) =  sqrt(3.0d0 / 5.0d0)
+    Omega%proj%xi_q(1) = -sqrt(3.0d0 / 5.0d0)
+    Omega%proj%xi_q(2) =  0.0d0
+    Omega%proj%xi_q(3) =  sqrt(3.0d0 / 5.0d0)
 
-    w_q(1) = 5.0d0 / 9.0d0
-    w_q(2) = 8.0d0 / 9.0d0
-    w_q(3) = 5.0d0 / 9.0d0
+    Omega%proj%w_q(1) = 5.0d0 / 9.0d0
+    Omega%proj%w_q(2) = 8.0d0 / 9.0d0
+    Omega%proj%w_q(3) = 5.0d0 / 9.0d0
 
     ! Map reference points to physical element
-    do q = 1, Nq
-      x_q(q) = J * xi_q(q) + (x_R + x_L) / 2.0d0
+    do q = 1, Omega%m%p
+      Omega%proj%x_q(q) = J * Omega%proj%xi_q(q) + (x_R + x_L) / 2.0d0
     end do
 
   end subroutine quadrature_points
