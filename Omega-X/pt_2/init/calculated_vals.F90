e707c7d06f0f777d3fd87985846ee62566290378
Physics_is_awesome <ajcason@outlook.com>
2025-12-05 15:29:11 -0600

    Update calculated_vals.F90

--- Omega-X/pt_2/init/calculated_vals.F90
+++ Omega-X/pt_2/init/calculated_vals.F90
@@ -3,76 +3,66 @@ module calc_vals
 
   implicit none
 contains
-  subroutine calculate_variables(nx, gamma, dx, rho_h, m_h, sigma_h, e, &
-                                 U, T_h, dT_h_dx, u_h, du_h_dx, s_h, T_S, &
-                                 p, eta_h, deta_h_dx, ny, nz)
-    ! Inputs
-    real(8), intent(in) :: gamma, dx
-    real(8), intent(in) :: rho_h(:,:,:), m_h(:,:,:), sigma_h(:,:,:), e(:,:,:)
-    integer, intent(in) :: nx, ny, nz
-    ! Outputs
-    real(8), intent(out) :: U(:,:,:), T_h(:,:,:), dT_h_dx(:,:,:)
-    real(8), intent(out) :: u_h(:,:,:), du_h_dx(:,:,:), s_h(:,:,:), T_S(:,:,:)
-    real(8), intent(out) :: p(:,:,:), eta_h(:,:,:), deta_h_dx(:,:,:)
+  subroutine calculate_variables(Omega)
+    type(Omega-X), intent(inout) :: Omega
+
   
     ! Local variables
-    real(8) :: x(nx,ny,nz)
+    real(8) :: x(Omega%m%nx, Omega%m%ny, Omega%m%nz)
     integer :: i, j, k
     
-
-
-
-
-    ! Internal energy (ideal EOS placeholder)
-    do k = 1, nz
-      do j = 1, ny
-        do i = 1, nx
-          U(i,j,k) = rho_h(i,j,k) ** (gamma - 1) * exp((gamma - 1) * s_h(i,j,k))
+    ! Eta/spesific entropy
+    do k = 1, Omega%m%nz
+      do j = 1, Omega%m%ny
+        do i = 1, Omega%m%nx
+          Omega%dv%eta_h(i,j,k) = Omega%S%sigma_h(i,j,k) / Omega%S%rho_h(i,j,k)
         end do
       end do
     end do
 
-    ! Temperature via midpoint derivative (vectorized helper)
-    call midpoint_derivative(U, sigma_h, T_h, ds=1.0d-3)
+    ! Internal energy (ideal EOS placeholder)
+    do k = 1, Omega%m%nz
+      do j = 1, Omega%m%ny
+        do i = 1, Omega%m%nx
+          Omega%dv%U(i,j,k) = Omega%S%rho_h(i,j,k) ** (Omega%const%gamma - 1) * exp((Omega%const%gamma - 1) * Omega%dv%eta(i,j,k))
+        end do
+      end do
+    end do
+
+    ! Temperature by internal energy derivitive of entropy via midpoint derivative 
+    call midpoint_derivative(Omega%dv%U, Omega%S%sigma_h, Omega%dv%T_h, ds=1.0d-3)
 
     ! Spatial derivative of temperature
-    call midpoint_derivative(T_h, x, dT_h_dx, dx)
+    call midpoint_derivative(Omega%dv%T_h, x, Omega%dv%dT_h_dx, Omega%m%dx)
 
     ! Velocity
-    do k = 1, nz
-      do j = 1, ny
-        do i = 1, nx
-          u_h(i,j,k) = m_h(i,j,k) / rho_h(i,j,k)
+    do k = 1, Omega%m%nz
+      do j = 1, Omega%m%ny
+        do i = 1, Omega%m%nx
+          Omega%dv%u_h(i,j,k) = Omega%S%m_h(i,j,k) / Omega%S%rho_h(i,j,k)
         end do
       end do
     end do
 
     ! Spatial derivative of velocity
-    call midpoint_derivative(u_h, x, du_h_dx, dx)
+    call midpoint_derivative(Omega%dv%u_h, x, Omega%dv%du_h_dx, Omega%m%dx)
 
-    ! Eta/spesific entropy
-    do k = 1, nz
-      do j = 1, ny
-        do i = 1, nx
-          eta_h(i,j,k) = sigma_h(i,j,k) / rho_h(i,j,k)
-        end do
-      end do
-    end do
+
 
     ! Ratio T/s
-    do k = 1, nz
-      do j = 1, ny
-        do i = 1, nx
-          T_S(i,j,k) = T_h(i,j,k) / eta(i,j,k)
+    do k = 1, Omega%m%nz
+      do j = 1, Omega%m%ny
+        do i = 1, Omega%m%nx
+          Omega%dv%T_S(i,j,k) = Omega%dv%T_h(i,j,k) / Omega%dv%eta(i,j,k)
         end do
       end do
     end do
 
     ! Pressure (ideal EOS placeholder)
-    do k = 1, nz
-      do j = 1, ny
-        do i = 1, nx
-          pressure(i,j,k) = (gamma - 1) * rho_h(i,j,k) ** gamma * exp((gamma - 1) * eta(i,j,k))
+    do k = 1, Omega%m%nz
+      do j = 1, Omega%m%ny
+        do i = 1, Omega%m%nx
+          Omega%dv%pressure(i,j,k) = (Omega%const%gamma - 1) * Omega%S%rho_h(i,j,k) ** Omega%const%gamma * exp((Omega%const%gamma - 1) * Omega%dv%eta(i,j,k))
         end do
       end do
     end do
@@ -80,7 +70,7 @@ contains
 
 
     ! Spatial derivative of eta
-    call midpoint_derivative(eta_h, x, deta_h_dx, dx)
+    call midpoint_derivative(Omega%dv%eta_h, x, Omega%dv%deta_h_dx, Omega%m%dx)
 
 
   
@@ -88,27 +78,7 @@ contains
 
   end subroutine calculate_variables
 
-  !======================
-  ! Spacial implicit mid-point
-  !======================
-  function space_discr(u, dx) result(dudx)
-    real(8), intent(in) :: u(:)
-    real(8), intent(in) :: dx
-    real(8) :: dudx(size(u))
-    integer :: i, n
 
-    n = size(u)
-
-    ! Interior points: central difference
-    do i = 2, n-1
-       dudx(i) = (u(i+1) - u(i-1)) / (2.0d0*dx)
-    end do
-
-    ! Boundaries: one-sided differences
-    dudx(1)   = (u(2) - u(1)) / dx
-    dudx(n)   = (u(n) - u(n-1)) / dx
-
-  end function space_discr
 
   !=========================================================
   ! General midpoint derivative helper
