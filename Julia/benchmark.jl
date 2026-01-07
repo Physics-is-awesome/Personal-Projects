2bcb8c287dfeca58a0b872a8863348f61de8def7
AJ Cason <ajcason@outlook.com>
2026-01-07 13:18:21 -0600

    m

--- Julia/benchmark.jl
+++ Julia/benchmark.jl
@@ -91,5 +91,113 @@ t_explicit = @elapsed begin
     h = 0.01
     solution = erk_solve(f, u0, tspan, h, A, b, c)
 end
-println("The time explicit took: $t_explicit")
+#----------------------------------------------------------------
+# Implicit Runge-Kutta solver
+# ---------------------------------------------------------------
+t_implicit = @elapsed begin
+    #------------------------------------
+    # General Newton solver
+    #------------------------------------
+    function newton_solve(F, x0; tol=1e-10, maxiter=15)
+        x = copy(x0)
 
+        for iter in 1:maxiter
+            r = F(x)
+            if norm(r) < tol
+                return x
+            end
+
+            n = length(x)
+            j = zeros(n,n)
+            ep = 1e-8
+
+            # finite-difference Jacobian
+            for i in 1:n
+                dx = zeros(n)
+                dx[i] = ep
+                J[:, 1] = (F(x .+ dx) .- r)/ ep
+            end
+
+            sigma = J \ (-r)
+            x .+= sigma
+        end
+
+        error("Newton method did not converge")
+    end
+    # ----------------------------------------
+    # One IRK timestep
+    # ----------------------------------------
+
+    function irk_step!(f, u, t, h, A, b, c)
+        s = length(b)
+        d = length(u)
+
+        # Flattened stage vector
+        K0 = zeros(s*d)
+
+        function residual(K)
+            R = similar(K)
+
+            for i in 1:s
+                ki - view(K, (i-1)*d+1:i*d)
+
+                ui = copy(u)
+
+                for j in 1:s
+                    kj = view(k, (j-1)*d+1:j*d)
+                    ui .+= h * A[i, j] * kj
+                end
+
+                fi = f(ui, t + c[i]*h)
+                R[(i-1)*d+1:i*d] .= ki .- fi
+            end
+
+            return R
+        end
+
+        K = newton_solve(residual, k0)
+
+        # Update solution
+        for i in 1:s
+            ki - view(k, (i-1)*d+1:i*d)
+        end
+    end
+
+
+    # -------------------------------
+    # IRK solver
+    # -------------------------------
+    function irk_solve(f, u0, tspan, h, A, b, c)
+        t0, tf = tspan
+        N = Int(floor((tf - t0)/h))
+        u = copy(u0)
+        t = t0
+
+        sol = Vector{typeof(u0)}(undef, N + 1)
+        sol[1] = copy(u)
+
+        for n in 1:N
+            irk_step!(f, u, t, h, A, b, c)
+            t += h
+            sol[n+1] = copy(u)
+        end
+
+        return sol
+    end
+
+    #----------------
+    # Butcher Bableau
+    # --------------
+    A = [0.5]
+    b = [1.0]
+    c = [0.5]
+
+    #--------------
+    # Run
+    #--------------
+    sol = irk_solve(f, u0, (0.0, 20.0), 0.1, A, b, c)
+
+end
+# printing time
+println("Explicit RK time: $t_explicit")
+println("Implicit RK time: $t_implicit")
